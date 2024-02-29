12aa4ef0b28928fd7a17de2e234027aba985844d
jahnavreddy <jahnavredy12@gmail.com>
2024-02-29 10:02:30 +0530

    databaseAdded

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const app = express()
 const PORT = process.env.PORT || 3000;
+const connectDB = require("./db")
+connectDB()
 
 app.listen(PORT,()=>{
   console.log(`Server is running at http://localhost:${PORT}`);
