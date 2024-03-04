7f8787d42425954c218b1d0169d0cbd5c2e1b5b8
jahnavreddy <jahnavredy12@gmail.com>
2024-03-04 17:36:05 +0530

    formAdded

--- Frontend/client/src/App.jsx
+++ Frontend/client/src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import LoginButton from './ComponentLogin/Login'
 import LogoutButton from './ComponentLogin/Logout'
 import Profile from './ComponentLogin/Profile'
 import Home from './Components/Home'
 import Navbar from './Components/Navbar'
+// import FormsUse from './Components/Donateform' 
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,6 +17,7 @@ function App() {
       <hr />
       <Profile /> */}
       <Navbar />
+      {/* <FormsUse /> */}
 
     </>
   )
