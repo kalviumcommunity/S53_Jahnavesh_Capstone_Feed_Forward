ffebbc225cbbd09158f5ec96de5731797987a19b
jahnavreddy <jahnavredy12@gmail.com>
2024-03-07 09:45:50 +0530

    added somes routes

--- Frontend/client/src/AllRoutes/AllRoutes.jsx
+++ Frontend/client/src/AllRoutes/AllRoutes.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import {Routes , Route} from "react-router-dom"
 import Home from "../Components/Home";
+import Contact from "../Components/Contact";
+import About from "../Components/About";
 
 function AllRoutes(){
   return(
-    <Routes>
-      <Route path="/" element = {<Home />}/>
-      
+     <Routes>
+       <Route path="/" element = {<Home />}/>
+       <Route path="/contact" element = {<Contact/>}/>
+       <Route path="/about" element={<About />}/>
     </Routes>
-  )
+   )
 }
+
+export default AllRoutes
---
+++ Frontend/client/src/Components/Contact.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Navbar from "./Navbar";
+
+export default function Contact(){
+  return(
+    <div>
+      <h1>Contact</h1>
+    </div>
+  )
+}
