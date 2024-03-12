928d731de207bdae70193d25ab7af261cb3c84f4
jahnavreddy <jahnavredy12@gmail.com>
2024-03-12 12:18:21 +0530

    updated css

--- Frontend/client/src/Components/Home.jsx
+++ Frontend/client/src/Components/Home.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import bagLogo from "../images/donate.png"
 
 function Home(){
   return(
     <div>
-      <img src={bagLogo} alt="baglogo" className="baglogo"/>
+
     </div>
   )
 }
