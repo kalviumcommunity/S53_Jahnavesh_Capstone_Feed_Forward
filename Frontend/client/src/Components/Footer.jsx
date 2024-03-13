c0f24867a3bf081bdb096cbb2aba1f86980adadb
jahnavreddy <jahnavredy12@gmail.com>
2024-03-13 11:53:19 +0530

    services footer done

--- Frontend/client/src/Components/Footer.jsx
+++ Frontend/client/src/Components/Footer.jsx
@@ -1,7 +1,10 @@
 import React from "react";
-import "../App.css";
+import "./Footer.css";
 import left_side_image from "../images/side design left.png" ;
 import right_side_image from "../images/side design right.png" ;
+import call_img from "../images/call symbol.png";
+import share_logo from "../images/share logo.png";
+import vehicle_logo from "../images/vehicle logo.png";
 
 export default function Footer(){
   return(
@@ -11,7 +14,21 @@ export default function Footer(){
       <div>
         <img src={left_side_image} alt="left-image" className="footer-left-image" />
         <img src={right_side_image} alt="right-image" className="footer-right-image" />
+        <div className="service-images">
+        <img src={call_img} alt="" />
+        <img src={share_logo} alt="" />
+        <img src={vehicle_logo} alt="" />
       </div>
+      </div>
+      <div className="service-names">
+        <p>WE SHIP</p>
+        <p className="provide-food">PROVIDE FOOD </p>
+        <p>CUSTOMER SERVICE</p>
+      </div>
+      {/* <div>
+        <p>We provide transportation of food</p>
+      </div> */}
+      
     </div>
   )
 }
