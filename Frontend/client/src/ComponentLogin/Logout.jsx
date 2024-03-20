eac71ad30c582edf3b64aa759bf4dae525a4e1cd
jahnavreddy <jahnavredy12@gmail.com>
2024-03-20 10:41:42 +0530

    given conditions for auth0

--- Frontend/client/src/ComponentLogin/Logout.jsx
+++ Frontend/client/src/ComponentLogin/Logout.jsx
@@ -5,8 +5,8 @@ const LogoutButton = () => {
   const { logout } = useAuth0();
 
   return (
-    <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
-      Log Out
+    <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })} className="logout">
+      LOGOUT
     </button>
   );
 };
