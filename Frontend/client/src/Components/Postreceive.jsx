92a7a234cd7d8ff597f940966bc7ef4dc9ff7269
jahnavreddy <jahnavredy12@gmail.com>
2024-03-30 15:01:51 +0530

    added routed to post recieve page

--- Frontend/client/src/Components/Postreceive.jsx
+++ Frontend/client/src/Components/Postreceive.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "../images/logo main edited.png";
+import {Link} from "react-router-dom"
  
 
 export default function Postreceive(){
@@ -8,13 +9,17 @@ export default function Postreceive(){
       <div className="confirmation">
         <p className="confirm_text">Are you sure to receive the food .</p>
         <div className="confirm_bottom_page"> 
+        <Link to={'/confirmation'}>
            <button className="confirm_btn">RECEIVE</button>
+        </Link>
+        <Link to={"/receive"}>
            <button className="goback_btn">GO BACK</button>
+        </Link>
            <div className="logo_postreceive">
               <img src={logo} className="logo" alt="Logo" />
               <p className="title_postreceive">
                 <span style={{ color: "white" }}>Feed</span>
-                <span style={{ color: "white" }}>Forward</span>{" "}
+                <span style={{ color: "white" }}>Forward</span>
               </p>
             </div>
         </div>
