0d29c949273813e2ddc17ddf3788587026bc57cb
jahnavreddy <jahnavredy12@gmail.com>
2024-04-09 15:44:21 +0530

    crom implemented

--- Backend/Schema.js
+++ Backend/Schema.js
@@ -1,49 +1,52 @@
-const mongoose = require("mongoose")
-const Schema1 = new mongoose.Schema({
-  Feedable_people : {
-    type : Number,
-    require : true
-  } ,
-  Location : {
-    type : String,
-    require : true
-  },
-  Contact : {
-    type : Number,
-    require : true
-  },
-  Food_details : {
-    type : String,
-    require : true
-  },
-  myFile : {
-    type : String,
-    require : true 
-  },
-  Donor_Name : {
-    type : String,
-    require : true
-  },
-  Donor_Email : {
-    type : String ,
-    require : true
-  }
+const mongoose = require("mongoose");
 
-})
+const Schema1 = new mongoose.Schema({
+  Feedable_people: {
+    type: Number,
+    required: true
+  },
+  Location: {
+    type: String,
+    required: true
+  },
+  Contact: {
+    type: Number,
+    required: true
+  },
+  Food_details: {
+    type: String,
+    required: true
+  },
+  myFile: {
+    type: String,
+    required: true
+  },
+  Donor_Name: {
+    type: String,
+    required: true
+  },
+  Donor_Email: {
+    type: String,
+    required: true
+  }
+}, {
+  timestamps: true
+});
 
 const Schema2 = new mongoose.Schema({
-  Contact : {
-    type : Number ,
-    require : true
+  Contact: {
+    type: Number,
+    required: true
   },
-  receiverPhoto : {
-    type : String ,
-    require : true
+  receiverPhoto: {
+    type: String,
+    required: true
   }
-})
+}, {
+  timestamps: true
+});
 
+const receiveSchema = mongoose.model("receive", Schema2);
+const donateSchema = mongoose.model("donate", Schema1);
 
-const receiveSchema = mongoose.model("receive",Schema2)
-const donateSchema = mongoose.model("donate",Schema1)
-
-module.exports = {donateSchema , receiveSchema}
+module.exports = { donateSchema, receiveSchema };
