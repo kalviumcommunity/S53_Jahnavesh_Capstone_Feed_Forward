0369f224b39e2d96fbf4ee2bed6023386134254d
jahnavreddy <jahnavredy12@gmail.com>
2024-04-15 14:09:26 +0530

    commented cron details in server

--- Backend/server.js
+++ Backend/server.js
@@ -17,25 +17,25 @@ app.use("/", router);
 
 connectDB();
 
-const Donate = mongoose.model("Donate", donateSchema);
+// const Donate = mongoose.model("Donate", donateSchema);
 
-const TTL_SECONDS = 3.5 * 60 * 60;
+// const TTL_SECONDS = 3.5 * 60 * 60;
 
-Donate.createIndex({ createdAt: 1 }, { expireAfterSeconds: TTL_SECONDS });
+// Donate.createIndex({ createdAt: 1 }, { expireAfterSeconds: TTL_SECONDS });
 
-cron.schedule('* * * * *', async () => {
-  const expiryTime = DateTime.now().minus({ hours: 3, minutes: 30 });
+// cron.schedule('* * * * *', async () => {
+//   const expiryTime = DateTime.now().minus({ hours: 3, minutes: 30 });
 
-  try {
-    const expiredDonations = await Donate.find({ createdAt: { $lt: expiryTime } });
-    for (const donation of expiredDonations) {
-      await donation.remove();
-      console.log(`Expired donation removed: ${donation._id}`);
-    }
-  } catch (error) {
-    console.error('Error removing expired donations:', error);
-  }
-});
+//   try {
+//     const expiredDonations = await Donate.find({ createdAt: { $lt: expiryTime } });
+//     for (const donation of expiredDonations) {
+//       await donation.remove();
+//       console.log(`Expired donation removed: ${donation._id}`);
+//     }
+//   } catch (error) {
+//     console.error('Error removing expired donations:', error);
+//   }
+// });
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
