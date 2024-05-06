16b1738ebb0a199e22b0e2303eda396efe7e9fd0
jahnavreddy <jahnavredy12@gmail.com>
2024-05-06 09:42:20 +0530

    updated app

--- Frontend/client/src/App.jsx
+++ Frontend/client/src/App.jsx
@@ -1,33 +1,14 @@
-import { useState } from 'react'
 import './App.css'
 import "./Components/Navbar.css"
 import "./Components/Footer.css"
-import LoginButton from './ComponentLogin/Login'
-import LogoutButton from './ComponentLogin/Logout'
-import Profile from './ComponentLogin/Profile'
-import Home from './Components/Home'
-import Navbar from './Components/Navbar'
 import AllRoutes from './AllRoutes/AllRoutes'
-import DonateForm from './Components/DonateForm'
-import Receive from './Components/Receive'
-import Footer from './Components/Footer'
-import About from './Components/About'
+import Dashboard from './Components/PreviousDonations'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      {/* <LoginButton />
-      <LogoutButton />
-      <hr />
-      <Profile /> */}
-      {/* <Navbar /> */}
-      {/* <Home/> */}
       <AllRoutes />
-      {/* <Receive /> */}
-      {/* <DonateForm /> */}
-
     </>
   )
 }
